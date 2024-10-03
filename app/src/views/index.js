10a7400708d76c750c307e65ea4852dcd8bb8c84
lemiso-professor <mathiaskipkoech45@gmail.com>
2024-10-03 10:12:45 +0300

    message

--- app/src/views/index.js
+++ app/src/views/index.js
@@ -1,7 +1,4 @@
 import HomeView from "./HomeView.vue";
-import DetailsView from "./DetailsView.vue";
-import LoginView from "./LoginView.vue";
-import PostsView from "./PostsView.vue";
-import PiniaTest from "./PiniaTest.vue";
 
-export { HomeView, DetailsView, LoginView, PostsView, PiniaTest };
+
+export { HomeView, };
